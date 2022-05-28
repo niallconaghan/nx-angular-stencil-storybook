869ec3d8f0a3a5235ae68fa82c8af26bb209d486
Niall Conaghan <niall.conaghan@gmail.com>
2022-05-28 13:55:09 +0100

    angular lib and application

--- packages/core-components/stencil.config.ts
+++ packages/core-components/stencil.config.ts
@@ -2,6 +2,13 @@ import { Config } from '@stencil/core';
 
 import { sass } from '@stencil/sass';
 
+const angularValueAccessorBindings: ValueAccessorConfig[] = [];
+
+import {
+  angularOutputTarget,
+  ValueAccessorConfig,
+} from '@stencil/angular-output-target';
+
 export const config: Config = {
   namespace: 'core-components',
   taskQueue: 'async',
@@ -21,5 +28,14 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+
+    angularOutputTarget({
+      componentCorePackage: '@nx-angular-stencil-storybook/core-components',
+      directivesProxyFile:
+        '../../../packages/core-components-angular/src/generated/directives/proxies.ts',
+      directivesArrayFile:
+        '../../../packages/core-components-angular/src/generated/directives/index.ts',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
   ],
 };
